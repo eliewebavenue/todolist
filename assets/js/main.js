c9b44b5bbfa5d899a690488c8c29d31648cc4fad
Elie <elie@webavenue.agency>
2023-10-19 16:48:51 +0200

    Suppression des commentaire

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ function addTodo() {
   if (todoText) {
     const todoEl = document.createElement("li");
     todoEl.innerText = todoText;
-    // Ajouter le bouton de modification
+
     const editBtn = document.createElement("button");
     editBtn.classList = 'edit';
     editBtn.innerText = "Modifier";
@@ -25,21 +25,21 @@ function editTodo(todoEl) {
   const input = document.createElement("input");
   input.type = "text";
   input.value = todoEl.innerText;
-  // Remplace le texte de la tâche par l'input
+
   todoEl.innerHTML = "";
   todoEl.appendChild(input);
   input.focus();
-  // Une fois la tâche modifiée (après appui sur "Entrée" ou clic en dehors)
+
   input.onblur = finishEditing;
   input.onkeydown = function(e) {
       if (e.key === "Enter") {
-          finishEditing.call(input); // Utilisez "call" pour définir "this" à l'intérieur de finishEditing
+          finishEditing.call(input); 
       }
   };
   function finishEditing() {
       const newText = this.value;
       todoEl.innerHTML = newText;
-      // Ajouter à nouveau le bouton de modification
+
       const editBtn = document.createElement("button");
       editBtn.innerText = "Modifier";
       editBtn.classList = 'edit';
